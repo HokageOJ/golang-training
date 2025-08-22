0fd8ff251838c4363439ac9f839625649221ccc7
HokageOj <rhokage2015@yandex.ru>
2025-08-22 20:41:38 +0300

    Done the task about the visit card

--- 1_variables/1_visit_card/main.go
+++ 1_variables/1_visit_card/main.go
@@ -13,5 +13,11 @@ import "fmt"
 // Мне 25 лет
 // Я живу в городе Москва
 func main() {
+	var name string = "Дмитрий"
+	var age int = 24
+	var city string = "Пенза"
 
+	fmt.Println("Меня зовут", name)
+	fmt.Println("Мне", age)
+	fmt.Println("Я живу в городе", city)
 }
