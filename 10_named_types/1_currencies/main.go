4c1fd04163386f41bfd9d6875193244a083b673a
HokageOj <rhokage2015@yandex.ru>
2025-08-24 21:17:23 +0300

    Done task about currencies converter

--- 10_named_types/1_currencies/main.go
+++ 10_named_types/1_currencies/main.go
@@ -25,10 +25,16 @@ func rubblesToDollars(money rubbles, rate rubbles) dollars {
 	return dollars(money / rate) 
 }
 
+func dollarsToRubbles(money dollars, rate rubbles) rubbles {
+	return rubbles(money * dollars(rate))
+}
+
 func main() {
 	var rubblesMoney rubbles = 1000.0
-	//var dollarsMoney dollars = 10.0
+	var dollarsMoney dollars = 1000.0
 
 	// result := rubblesMoney + dollarsMoney
+
 	fmt.Println(rubblesToDollars(rubblesMoney, rubbles(90)))
+	fmt.Println(dollarsToRubbles(dollarsMoney, rubbles(82)))
 }
