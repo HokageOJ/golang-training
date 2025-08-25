8c458edd06ebbb8292fac6c44072b46b7bd46e45
HokageOj <rhokage2015@yandex.ru>
2025-08-25 20:38:07 +0300

    Done task about pet age counter

--- 1_variables/2_pet_age_counter/main.go
+++ 1_variables/2_pet_age_counter/main.go
@@ -11,5 +11,9 @@ import "fmt"
 //
 // Через 2 года вашей собаке будет 6 лет/года
 func main() {
+	var currentDogAge int = 4
+	var yearsLater int = 5
+	var futureDogAge = currentDogAge + yearsLater
 
+	fmt.Println("Через", yearsLater, "года", "вашей собаке будет", futureDogAge, "лет/года")
 }
