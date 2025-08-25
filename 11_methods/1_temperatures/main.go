d0dc07c206a5f9055b466b72e0676dfee266f946
HokageOj <rhokage2015@yandex.ru>
2025-08-25 21:42:34 +0300

    Add to Kelvin method for Celsius type

--- 11_methods/1_temperatures/main.go
+++ 11_methods/1_temperatures/main.go
@@ -19,8 +19,13 @@ type Kelvin float64
 func (temp Celsius) String() string {
 	return fmt.Sprintf("%.1f°С", temp)
 } 
+
 func (temp Celsius) ToFahrenheit() Fahrenheit {
 	return Fahrenheit(temp * 9/5 + 32)
+}
+
+func (temp Celsius) ToKelvin() Kelvin {
+	return Kelvin(temp + 273.15)
 }
 
 func main() {
@@ -28,4 +33,5 @@ func main() {
 	
 	fmt.Println(celsiusTemp.String())
 	fmt.Println(celsiusTemp.ToFahrenheit())
+	fmt.Println(celsiusTemp.ToKelvin())
 }
