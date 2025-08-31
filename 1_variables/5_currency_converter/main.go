81f97c315fda21071de36b276ecef75bd534c840
HokageOj <rhokage2015@yandex.ru>
2025-08-31 21:18:51 +0300

    Done task about currency converter

--- 1_variables/5_currency_converter/main.go
+++ 1_variables/5_currency_converter/main.go
@@ -11,5 +11,8 @@ import "fmt"
 //
 // 4500 рублей будет 50 долларов
 func main() {
+	var rubbles float64 = 4500
+	var exchangeRate float64 = 90
 
+	fmt.Printf("При обмене %.2f рубля(ей) по курсу %.2f рублей за доллар, получиться: %.2f долларов\n", rubbles, exchangeRate, rubbles/exchangeRate)
 }
