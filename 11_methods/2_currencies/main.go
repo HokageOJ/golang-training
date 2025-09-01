966aaef08c7c47a17a06d35e8114b4cd30e098cf
HokageOj <rhokage2015@yandex.ru>
2025-09-01 18:49:43 +0300

    Add methods for dollars type

--- 11_methods/2_currencies/main.go
+++ 11_methods/2_currencies/main.go
@@ -28,16 +28,43 @@ func (money rubbles) toEuro(rate rubbles) euro {
 }
 
 func (money *rubbles) addTax(percent float64) {
-	*money = *money + (*money * rubbles(percent / 10))
+	*money = *money + (*money * rubbles(percent / 100))
+}
+
+func (money dollars) string() string {
+	return fmt.Sprintf("$%.2f", money)
+}
+
+func (money dollars) toRubbles(rate dollars) rubbles {
+	return rubbles(money / rate)
+}
+
+func (money dollars) toEuro(rate dollars) euro {
+	return euro(money * rate)
+}
+
+func (money *dollars) addTax(percent float64) {
+	*money = *money + (*money * dollars(percent / 100))
 }
 
 func main() {
 	var money rubbles = 100.00
 
-	fmt.Println(money.string())
-	fmt.Println(money.toDollars(90.00))
-	fmt.Println(money.toEuro(100))	
+	fmt.Println("Рубли:", money.string())
+	fmt.Println("Рубли в доллары:", money.toDollars(90.00).string())
+	fmt.Println("Рубли в евро:", money.toEuro(100))	
 	
 	money.addTax(20) 
-	fmt.Println(money)
+	fmt.Println("Добавлен налог 20% к рублям:", money.string())
+
+	fmt.Println()
+
+	var cash dollars = 100.00
+
+	fmt.Println("Доллары:", cash.string())
+	fmt.Println("Доллары в рубли:", cash.toRubbles(0.012).string())
+	fmt.Println("Доллары в евро:", cash.toEuro(0.85))
+
+	cash.addTax(20)
+	fmt.Println("Добавлен налог 20% к евро:", cash.string())
 }
