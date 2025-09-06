7640e9ebb13af769c03e7889b490abe16142ac3e
HokageOj <rhokage2015@yandex.ru>
2025-09-06 21:06:18 +0300

    Done task about temp in countries

--- 1_variables/6_temp_in_countries/main.go
+++ 1_variables/6_temp_in_countries/main.go
@@ -12,5 +12,11 @@ import "fmt"
 // В стране Норвегия сейчас 10 °C
 // В стране Испания сейчас 28 °C
 func main() {
+	var countries string = "Норвегия"
+	var countries2 string = "Испания"
+	var tempNorway int = 10
+	var tempSpain int = 28
 
+	fmt.Printf("В стране %s сейчас %d °C\n", countries, tempNorway)
+	fmt.Printf("В старне %s сейчас %d °C\n", countries2, tempSpain)
 }
