6068088f94742f4475d39f7abef330b0dfd262d8
HokageOj <rhokage2015@yandex.ru>
2025-09-08 21:51:13 +0300

    Done task about min/max

--- 7_for/5_min_max/main.go
+++ 7_for/5_min_max/main.go
@@ -19,4 +19,22 @@ import "fmt"
 
 func main() {
 	var numbers [10]int = [10]int{5, 12, 8, 3, 17, 9, 1, 14, 6, 10}
+
+	var min int = numbers[0]
+	var max int = numbers[0]
+
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] < min {
+			min = numbers[i]
+		} 
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] > max {
+			max = numbers[i]
+		}
+	}
+
+	fmt.Println("Min: ", min)
+	fmt.Println("Max: ", max)
 }
