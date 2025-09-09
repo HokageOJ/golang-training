6da1adba53c59c767ae2ca2f91a8e8df9cac7232
HokageOj <rhokage2015@yandex.ru>
2025-09-09 20:54:04 +0300

    Done task about search in array

--- 7_for/6_search/main.go
+++ 7_for/6_search/main.go
@@ -25,5 +25,26 @@ import "fmt"
 */
 
 func main() {
-	var numbers [10]int = [10]int{42, 7, 15, 23, 2, 11, 30, 4, 19, 8}
+	var numbers [10]int = [10]int{42, 4, 15, 23, 2, 11, 30, 4, 19, 42}
+
+	fmt.Printf("Массив: %d\n", numbers)
+
+	var search int
+
+	fmt.Print("Введите число для поиска: ")
+	fmt.Scan(&search)
+
+	var isFound bool
+
+	for i := 0; i < len(numbers); i++ {
+		if search == numbers[i] {
+			fmt.Printf("Число %d найдено! Индекс элемента: %d\n", search, i)
+			isFound = true
+			break
+		} 
+	}
+	
+	if !isFound {
+		fmt.Printf("Число %d не найдено в массиве!\n", search)
+	}
 }
